dea7962629d5169ce9c610d52c499258d1864bdd
alacambra <alacambra@gmail.com>
2025-07-18 11:24:10 +0200

    new ts api models

---
+++ client/src/api/models/index.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './CreateDocumentRequest';
+export * from './DocumentRequest';
+export * from './KnowledgeUnit';
+export * from './KnowledgeUnitRequest';
